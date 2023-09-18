c6e6dc8cb57da9870320479c8a15a8f40495bde1
Nikhil <nsjniksam@gmail.com>
2023-09-18 17:51:54 +0530

    added responsiveness

--- LoginRegisterportal/portal.js
+++ LoginRegisterportal/portal.js
@@ -1,1 +1,8 @@
-var  a = ge
+
+const rname=document.querySelector('#rname');
+const rpassword=document.getElementById('rpassword');
+const email=document.getElementById('email');
+const rcpassword=document.getElementById('rcpassword');
+const lname=document.querySelector('#lname');
+const lpassword=document.getElementById('lpassword');
+
