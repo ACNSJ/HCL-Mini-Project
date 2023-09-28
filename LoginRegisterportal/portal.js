abe16d15ea1053f2d690972c7e145295f406fccf
Nikhil <nsjniksam@gmail.com>
2023-09-28 23:09:05 +0530

    Backend

--- LoginRegisterportal/portal.js
+++ LoginRegisterportal/portal.js
@@ -1,8 +1,6 @@
-
 const rname=document.querySelector('#rname');
 const rpassword=document.getElementById('rpassword');
 const email=document.getElementById('email');
 const rcpassword=document.getElementById('rcpassword');
 const lname=document.querySelector('#lname');
 const lpassword=document.getElementById('lpassword');
-
